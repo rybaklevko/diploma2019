e3756ed2080fa376da4651c053cbf0b43a8e3a58
Rybak Lev <rybaklevo@ukr.net>
2019-09-09 00:29:15 +0300

    Added hardcoded devices

--- pages/devices_page.py
+++ pages/devices_page.py
@@ -1,7 +1,10 @@
 from app import app
 from flask import render_template
 
+# TODO data for device have to be read from DataBase
+device_list = [{'name': 'rybak_device', 'ip': '2', 'userList': ['Lev Rybak', 'DSA dsa'], 'address' : 'Konovalsta str'}]
+
 
 @app.route('/devices_page', methods=['GET', 'POST'])
 def devices_page(error=''):
-    return render_template("devices.html", error=error)
+    return render_template("devices.html", devices=device_list, error=error)
