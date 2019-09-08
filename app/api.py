f36a11b3d085cadfd7bc264d7aec3fd54b5a6b6b
Rybak Lev <rybaklevo@ukr.net>
2019-09-08 19:53:59 +0300

    Added control of signing in/logging in

--- app/api.py
+++ app/api.py
@@ -28,33 +28,51 @@ def log_user_in():
         username = request.form['email']
         password = request.form['password']
         print(username+password)
+
+
+        if (username == '' or password == ''):
+            error = "Both user name and password are required!"
+            return log_in_page(error)
+
         #response = get_user_from_db(username, password);
         #status_code = response.status_code
-
 
         status_code = 400
         if (username == "admin@admin"):
             if (password == "admin123"):
                 status_code = 200
 
-
-
         if status_code == 200:
             return user_main_page()
         else:
-            return "ERROR"
+            error="User's credentials are wrong!"
+            return log_in_page(error)
 
     else:
         return "Not post request!"
 
 
-@app.route("/register_user", methods=['POST','GET'])
+@app.route("/register_user", methods=['POST', 'GET'])
 def register_user():
-    #1)add user into db
 
-    #2)log in
-    return log_user_in()
+    if request.method == 'POST':
+        email = request.form['email']
+        name = request.form['name']
+        password = request.form['password']
+        print(email + name + password)
 
+
+        if (email == '' or password == '' or name == ''):
+            error = "All these fields are required!"
+            return registration_page(error)
+
+            #1)add user into db
+
+            # #2)log in
+            return log_user_in()
+
+    else:
+        return "Not post request!"
 
 
 @app.route('/profile_page', methods=['GET'])
@@ -63,15 +81,15 @@ def profile_page():
 
 
 @app.route('/log_in_page', methods=['GET'])
-def log_in_page():
-    return render_template("login.html")
+def log_in_page(error=''):
+    return render_template("login.html", error=error)
 
 
-@app.route('/registration_page', methods=['POST','GET'])
-def registration_page():
-    return render_template('signup.html')
+@app.route('/registration_page', methods=['POST', 'GET'])
+def registration_page(error=''):
+    return render_template('signup.html', error=error)
 
 
-@app.route('/user_main_page', methods=['POST','GET'])
+@app.route('/user_main_page', methods=['POST', 'GET'])
 def user_main_page():
     return render_template('user_main_page.html')
