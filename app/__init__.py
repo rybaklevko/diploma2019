95bd8861b19e41f19b79eab617bb4b7ca0b68093
Rybak Lev <rybaklevo@ukr.net>
2019-09-11 00:12:21 +0300

    Added static folder control

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 
 from flask import Flask
 
-app = Flask(__name__, template_folder='../templates/')
+app = Flask(__name__, template_folder='../templates/', static_folder='../static')
 
 from app import api
 
