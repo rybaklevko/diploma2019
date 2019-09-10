c2f823995a1e6eec3003c3c90aa05f30877195da
Rybak Lev <rybaklevo@ukr.net>
2019-09-10 23:28:48 +0300

    Updated devices page, updated templated for users and device page

--- pages/devices_page.py
+++ pages/devices_page.py
@@ -2,7 +2,8 @@ from app import app
 from flask import render_template
 
 # TODO data for device have to be read from DataBase
-device_list = [{'name': 'rybak_device', 'ip': '2', 'userList': ['Lev Rybak', 'DSA dsa'], 'address' : 'Konovalsta str'}]
+device_list = [{'name': 'rybak_device', 'ip': '2', 'userList': {'Lev Rybak', 'DSA dsa'}, 'address' : 'Konovalsta str'},
+               {'name': 'rybak_device', 'ip': '2', 'userList': {'Lev Rybak', 'DSA dsa'}, 'address': 'Konovalsta str'}]
 
 
 @app.route('/devices_page', methods=['GET', 'POST'])
