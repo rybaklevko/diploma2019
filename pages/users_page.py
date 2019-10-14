860cc200b80bf4cd92c3e7e61a22d823b92a0c25
Rybak Lev <rybaklevo@ukr.net>
2019-10-14 17:58:37 +0300

    Added users

--- pages/users_page.py
+++ pages/users_page.py
@@ -3,8 +3,8 @@ from flask import render_template
 
 
 # TODO data for device have to be read from DataBase
-users_list = [{'firstName': 'Lev', 'secondName': 'Rybak', 'imagesList':{'lev1.jpg','lev2.jpg','lev3.jpg'}}]
-    #,               {'firstName': 'Oleg', 'secondName': 'Pronko'}]
+users_list = [{'firstName': 'Roman', 'secondName': 'Savuch', 'imagesList':{'jeff1.jpg','jeff2.jpg','jeff3.jpg'}},
+              {'firstName': 'Lev', 'secondName': 'Rybak', 'imagesList': {'lev1.jpg', 'lev2.jpg', 'lev3.jpg'}}]
 
 @app.route('/users_page', methods=['GET', 'POST'])
 def users_page(error=''):
