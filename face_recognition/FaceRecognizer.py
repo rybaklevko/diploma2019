dfb894e3f37788fbd6b8fa85e1c10150626048b0
Rybak Lev <rybaklevo@ukr.net>
2019-11-23 16:35:34 +0200

    Updated face reco

--- face_recognition/FaceRecognizer.py
+++ face_recognition/FaceRecognizer.py
@@ -1,5 +1,15 @@
 import face_recognition
 
+images = []
+user_image1 = None
+user_image2 = None
+user_image3 = None
+
+encoded_images = []
+
+users_list = [{'firstName': 'Jeff', 'secondName': 'Amazon', 'imagesList':['jeff1.jpg','jeff2.jpg','jeff3.jpg']},
+              {'firstName': 'Lev', 'secondName': 'Rybak', 'imagesList': ['lev1.jpg', 'lev2.jpg', 'lev3.jpg']}]
+
 
 def compare_faces(known_faces_encoded, input_image):
     index = -1
@@ -11,31 +21,36 @@ def compare_faces(known_faces_encoded, input_image):
 
 
 # Load the jpg file into a numpy array
-lev_image = face_recognition.load_image_file("lev.jpg")
-obama_image = face_recognition.load_image_file("obama.jpg")
+def load_images(user):
+    global user_image1
+    user_image1=face_recognition.load_image_file(user['imagesList'][0])
+    global user_image2
+    user_image2=face_recognition.load_image_file(user['imagesList'][1])
+    global user_image3
+    user_image3 = face_recognition.load_image_file(user['imagesList'][2])
 
+def encode_image():
+    user_face_encoding1 = face_recognition.face_encodings(user_image1)[0]
+    user_face_encoding2 = face_recognition.face_encodings(user_image2)[0]
+    user_face_encoding3 = face_recognition.face_encodings(user_image3)[0]
+    global encoded_images
+    encoded_images = [
+        user_face_encoding1,
+        user_face_encoding2,
+        user_face_encoding3
+    ]
 
-lev_face_encoding = face_recognition.face_encodings(lev_image)[0]
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+    # for image in images:
+    #     encoded_images.append(face_recognition.load_image_file(image)[0])
 
-known_face_encodings = [
-   obama_face_encoding,
-   lev_face_encoding
-]
-
-input_image = face_recognition.load_image_file("lev1.jpg")
-input_image_encoding = face_recognition.face_encodings(input_image)[0]
-
-known_face_names = [
-   "Barack Obama",
-   "Lev Rybak"
-]
-
-name_index = compare_faces(known_faces_encoded=known_face_encodings, input_image=input_image_encoding)
-if -1 != name_index:
-    print(known_face_names[name_index])
-else:
-    print("Face is not found")
-
-
-
+def base_function():
+    for user in users_list:
+        load_images(user)
+        encode_image()
+        input_image = face_recognition.load_image_file("lev1.jpg")
+        input_image_encoding = face_recognition.face_encodings(input_image)[0]
+        name_index = compare_faces(known_faces_encoded=encoded_images, input_image=input_image_encoding)
+        if -1 != name_index:
+            print(user['firstName'] + ' ' + user['secondName'])
+        else:
+            print("Face is not found")
