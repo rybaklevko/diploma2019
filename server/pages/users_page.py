98d4387da86ecd85dd1994d4900fc68383f9c667
Rybak Lev <rybaklevo@ukr.net>
2019-12-01 00:50:16 +0200

    Added test image for users page

--- server/pages/users_page.py
+++ server/pages/users_page.py
@@ -1,10 +1,13 @@
 from app import app
 from flask import render_template
 
+import base64
+from PIL import Image
+from io import BytesIO
 
 # TODO data for device have to be read from DataBase
-users_list = [{'firstName': 'Roman', 'secondName': 'Savuch', 'imagesList':{'jeff1.jpg','jeff2.jpg','jeff3.jpg'}},
-              {'firstName': 'Lev', 'secondName': 'Rybak', 'imagesList': {'lev1.jpg', 'lev2.jpg', 'lev3.jpg'}}]
+users_list = [{'firstName': 'Roman', 'secondName': 'Savuch', 'imagesList': {'jeff1.jpg','jeff2.jpg','jeff3.jpg'}},
+               {'firstName': 'Lev', 'secondName': 'Rybak', 'imagesList': {'lev1.jpg', 'lev2.jpg', 'lev3.jpg'}}]
 
 
 @app.route('/add_user', methods=['POST'])
@@ -16,4 +19,6 @@ def add_new_user(error=''):
 
 @app.route('/users_page', methods=['GET', 'POST'])
 def users_page(error=''):
-    return render_template("users.html", users=users_list, error=error)
+    with open("C:\\Users\\Lev\\PycharmProjects\\face_recognition_lock_server\\server\\DB\\images\\lev1.jpg", "rb") as image_file:
+        img = base64.b64encode(image_file.read()).decode('ascii')
+    return render_template("users.html", users=users_list, img_data=img, error=error)
