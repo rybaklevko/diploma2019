11009a5699396d8f740d8dc4da166c739c87f3b0
Rybak Lev <rybaklevo@ukr.net>
2019-12-05 00:06:04 +0200

    Added images button adding

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 from flask import Flask
-
+UPLOAD_FOLDER = 'C:\\Users\\Lev\\PycharmProjects\\face_recognition_lock_server\\server\\DB\\images\\'
 app = Flask(__name__, template_folder='../templates/', static_folder='../static')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 from app import api
 
--- server/pages/users_page.py
+++ server/pages/users_page.py
@@ -1,7 +1,8 @@
 from app import app
 from flask import render_template, redirect, request
 
-import base64
+import base64, os
+from werkzeug.utils import secure_filename
 
 # TODO data for device have to be read from DataBase
 users_list = [{'user_id':'1','firstName': 'Roman', 'secondName': 'Savuch', 'imagesList': {'rom1.jpg','rom2.jpg','rom3.jpg'}},
@@ -15,28 +16,48 @@ class UserWebPage():
         self.imageList = imageList
 
 
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.route('/add_user', methods=['GET', 'POST'])
 def add_user(error=''):
     # add user, read from requests form and add into users_list
     try:
         if request.method == 'POST':
             print("Is post method")
+            if 'file' not in request.files:
+                print('No file part')
+
             second_name = request.form["second_name"]
             first_name = request.form["first_name"]
-            image_list = request.form["image_list"]
+            image_file = request.files["image_list"]
+
+            if image_file.filename == '':
+                print('No selected file')
+            if image_file and allowed_file(image_file.filename):
+                print(image_file.filename)
+                filename = secure_filename(image_file.filename)
+                full_path = (os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                image_file.save(full_path)
+
             user = dict()
             user['used_id'] = 3
             user['firstName'] = first_name
             user['secondName'] = second_name
-            user['imagesList'] = image_list
-            #
+            user['imagesList'] = []
+            user['imagesList'].append(image_file.filename)
+
             users_list.append(user)
             return redirect('/users_page')
         else:
             print("Is not post method!!")
     except KeyError:
         print("No such key in form")
-    print(request.method)
+
     return render_template("users_adding.html",  error=error)
 
 
@@ -47,7 +68,7 @@ def users_page(error=''):
         images = []
         images_name = []
         for image in user['imagesList']:
-            file_name = "C:\\Users\\Lev\\PycharmProjects\\face_recognition_lock_server\\server\\DB\\images\\" + image
+            file_name = (os.path.join(app.config['UPLOAD_FOLDER'], image))
             with open(file_name, "rb") as image_file:
                 images_name.append(file_name)
                 img = base64.b64encode(image_file.read()).decode('ascii')
