93f7ee762c58f3a934c16d5afbe1995a89e39430
Rybak Lev <rybaklevo@ukr.net>
2019-12-07 16:27:08 +0200

    Added message boxes

--- locker/requester.py
+++ locker/requester.py
@@ -1,5 +1,8 @@
 # importing the requests library
 import requests
+import tkinter
+from tkinter import messagebox
+
 
 # defining the api-endpoint
 API_ENDPOINT = "http://127.0.0.1:5000/web_camera_image"
@@ -17,9 +20,13 @@ def send_image(path):
 
         if r.ok:
                 print("Opening doors")
+                root = tkinter.Tk()
+                root.withdraw()
+                messagebox.showinfo("Face has been found!", "Find user : %s. Opening door...")
                 return True
         else:
                 print("Face not found - door still closed")
+                messagebox.showerror("Face hasn't been found!", "Face not found - door still closed")
                 return False
         return False
 
